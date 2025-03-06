8a85c65bcc25e9fd4fdc4aea7397596a90cbfb18
Toni Haryanto <toha.samba@gmail.com>
2025-03-06 11:24:04 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php include('partials/header.php') ?>
+
+<div id="admin_list_tagihan" x-data="admin_list_tagihan()">
+
+    <div class="app-header">
+        <div class="left">
+            <a href="javascript:void()" onclick="history.back()">
+                <i class="bi bi-arrow-left"></i>
+            </a>
+        </div>
+        <div class="page-title">Header</div>
+        <div class="right">
+            <a href="/add">
+                <i class="bi bi-plus-circle"></i>
+            </a>
+        </div>
+    </div>
+
+    <div class="app-container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit cumque fugit rem laboriosam natus iure libero nobis consequatur magni rerum dignissimos, quod quo voluptatibus odio animi ratione nihil aut.</p>
+    </div>
+
+    <div class="app-bottommenu no-border shadow-lg">
+        <a href="/" id="bottommenu-item" class="item active">
+            <div class="col">
+                <i class="bi bi-house"></i> <strong>Beranda</strong>
+            </div>
+        </a>
+        <a href="/courses" id="bottommenu-item" class="item">
+            <div class="col">
+                <i class="bi bi-book"></i> <strong>Courses</strong>
+            </div>
+        </a>
+        <a href="/feeds" id="bottommenu-item" class="item">
+            <div class="col">
+                <i class="bi bi-newspaper"></i> <strong>Kabar</strong>
+            </div>
+        </a>
+        <a href="/profile" id="bottommenu-item" class="item">
+            <div class="col">
+                <i class="bi bi-person-circle"></i> <strong>Akun</strong>
+            </div>
+        </a>
+    </div>
+</div>
+
+<?php include('partials/footer.php') ?>
